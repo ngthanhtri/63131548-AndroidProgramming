d34551e03bca205bbe038b20e56b8bb2442ce2f2
thanhtri2112 <thanhtri21120309@gmail.com>
2024-03-24 17:56:10 +0700

    MainActivity viết lệnh sự kiện các nút ấn

--- Cau2_AppTracNghiemLop1/app/src/main/java/ntu/thanhtri63131548/MainActivity.java
+++ Cau2_AppTracNghiemLop1/app/src/main/java/ntu/thanhtri63131548/MainActivity.java
@@ -23,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private Button btnsau;
     private Button btntruoc;
 
+    private List<CauHoi> dscauhoi;
+    private int giatricauhoi = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +37,67 @@ public class MainActivity extends AppCompatActivity {
         btnsau = findViewById(R.id.btnsau);
         btntruoc = findViewById(R.id.btntruoc);
 
+        dscauhoi = taodscauhoi();
+
+        hienthich(dscauhoi.get(giatricauhoi));
+
+        btnnop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kiemtrakq(dscauhoi.get(giatricauhoi));
+            }
+        });
+
+        btnsau.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (giatricauhoi < dscauhoi.size() - 1) {
+                    giatricauhoi++;
+                    hienthich(dscauhoi.get(giatricauhoi));
+                }
+            }
+        });
+
+        btntruoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (giatricauhoi > 0) {
+                    giatricauhoi--;
+                    hienthich(dscauhoi.get(giatricauhoi));
+                }
+            }
+        });
+    }
+
+
+
+    private void hienthich(CauHoi c) {
+        TVcauhoi.setText(c.ChonCauHoi());
+        rdgrcautraloi.removeAllViews();
+        for (int i = 0; i < c.ChonDapAn().size(); i++) {
+            RadioButton rb = new RadioButton(this);
+            rb.setText(Integer.toString(c.ChonDapAn().get(i)));
+            rdgrcautraloi.addView(rb);
+        }
+    }
+
+
+    private void kiemtrakq(CauHoi question) {
+        int id = rdgrcautraloi.getCheckedRadioButtonId();
+        if (id != -1) {
+            RadioButton selectedRadioButton = findViewById(id);
+            int n = Integer.parseInt(selectedRadioButton.getText().toString());
+            if (n == question.ChonCauTraLoi()) {
+                Toast.makeText(MainActivity.this, "Correct!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Incorrect!", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(MainActivity.this, "Please select an answer!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
+
 
 
 
