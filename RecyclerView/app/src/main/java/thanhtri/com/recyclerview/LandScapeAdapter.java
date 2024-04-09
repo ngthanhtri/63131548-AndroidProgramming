d05ba32776e4f529bf4ff0dba5169f3fdf0387dd
thanhtri2112 <thanhtri21120309@gmail.com>
2024-04-09 14:09:12 +0700

    Ví dụ RecyclerView

---
+++ RecyclerView/app/src/main/java/thanhtri/com/recyclerview/LandScapeAdapter.java
@@ -0,0 +1,73 @@
+package thanhtri.com.recyclerview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.zip.Inflater;
+
+public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.ItemLandHolder> {
+    Context context;
+    ArrayList<LandScape> lsData;
+
+    public LandScapeAdapter(Context context, ArrayList<LandScape> lsData) {
+        this.context = context;
+        this.lsData = lsData;
+    }
+
+    @NonNull
+    @Override
+    public ItemLandHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater cai_bom = LayoutInflater.from(context);
+        View vItem = cai_bom.inflate(R.layout.item_land, parent, false);
+        ItemLandHolder viewholderCreated = new ItemLandHolder(vItem);
+        return viewholderCreated;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemLandHolder holder, int position) {
+        LandScape landScapeHienThi = lsData.get(position);
+        String caption = landScapeHienThi.getLandCation();
+        String tenFileAnh = landScapeHienThi.getLandImageFileName();
+        holder.tvCaption.setText(caption);
+        String packageName = holder.itemView.getContext().getPackageName();
+        int imageID = holder.itemView.getResources().getIdentifier(tenFileAnh, "mipmap",packageName);
+        holder.ivLandSCape.setImageResource(imageID);
+    }
+
+    @Override
+    public int getItemCount() {
+        return lsData.size();
+    }
+
+    class ItemLandHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView tvCaption;
+        ImageView ivLandSCape;
+
+        public ItemLandHolder(@NonNull View itemView) {
+            super(itemView);
+            tvCaption = itemView.findViewById(R.id.textViewCaption);
+            ivLandSCape = itemView.findViewById(R.id.imageViewLand);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int pos = getAdapterPosition();
+            LandScape n = lsData.get(pos);
+            String ten = n.getLandCation();
+            String tenFile = n.getLandImageFileName();
+            String thongbao = "Ban vua click vao : "+ten;
+            Toast.makeText(v.getContext(),thongbao,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
---
+++ RecyclerView/app/src/main/java/thanhtri/com/recyclerview/MainActivity.java
@@ -0,0 +1,58 @@
+package thanhtri.com.recyclerview;
+
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> recycleViewDatas;
+    RecyclerView recyclerViewLandScape;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        recycleViewDatas = getDataForReCyClerView();
+        recyclerViewLandScape = findViewById(R.id.recyclerLand);
+        /* RecyclerView.LayoutManager layoutLinear  = new LinearLayoutManager(this);
+        recyclerViewLandScape.setLayoutManager(layoutLinear);
+        RecyclerView.LayoutManager layoutLinearHorizonal  = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewLandScape.setLayoutManager(layoutLinearHorizonal); */
+        RecyclerView.LayoutManager layoutLinearGrid  = new GridLayoutManager(this,2);
+        recyclerViewLandScape.setLayoutManager(layoutLinearGrid);
+
+        landScapeAdapter = new LandScapeAdapter(this, recycleViewDatas);
+        recyclerViewLandScape.setAdapter(landScapeAdapter);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    ArrayList<LandScape> getDataForReCyClerView() {
+        ArrayList<LandScape> dsDuLieu = new ArrayList<LandScape>();
+        LandScape landScape1 = new LandScape("ronaldo", "Ronaldo siuuuuu");
+        dsDuLieu.add(landScape1);
+        dsDuLieu.add(new LandScape("messi", "Messi"));
+        dsDuLieu.add(new LandScape("mbappe", "Mbappe"));
+        dsDuLieu.add(new LandScape("neymar", "Neymar"));
+        return dsDuLieu;
+    }
+}
